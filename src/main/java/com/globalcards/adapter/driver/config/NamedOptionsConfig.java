d9fd201f91cda840d8bb7af779c50901629433b3
Reis, Lucas Barbosa Plácido <lucas.barbosa.p.reis@accenture.com>
2023-03-14 19:30:23 -0300

    feat: adicionado projeto funcionando com healthcheck

--- src/main/java/com/globalcards/adapter/driver/config/NamedOptionsConfig.java
+++ src/main/java/com/globalcards/adapter/driver/config/NamedOptionsConfig.java
@@ -2,19 +2,18 @@ package com.globalcards.adapter.driver.config;
 
 import io.smallrye.common.annotation.Identifier;
 import io.vertx.rabbitmq.RabbitMQOptions;
-
 import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.Produces;
 
 @ApplicationScoped
 public class NamedOptionsConfig {
-    
     @Produces
     @Identifier("options")
     public static RabbitMQOptions getNamedOptions() {
             return new RabbitMQOptions()
                     .setTrustAll(true)
                     .setSsl(true)
-                    .setAutomaticRecoveryOnInitialConnection()
+                    .setAutomaticRecoveryOnInitialConnection(true)
+                    .setAutomaticRecoveryEnabled(false);
     }
 }
